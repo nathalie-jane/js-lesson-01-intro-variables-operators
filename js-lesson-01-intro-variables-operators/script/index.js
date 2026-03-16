7803297600790dc00a2312e37f0e22bd528e14b9
nathalie-jane <nathalie.zheng@gmail.com>
2026-03-16 12:09:29 +0100

    feat: add exercise 1 – store and print a message
    - Create variable for message string
    - Output message using console.log()

---
+++ js-lesson-01-intro-variables-operators/script/index.js
@@ -0,0 +1,10 @@
+/* ====================================================
+    EXERCISE 1
+    Store and print a message
+======================================================= */
+
+// Store message in a variable
+let message = "Hello, World!";
+
+// Output message
+console.log(message);
